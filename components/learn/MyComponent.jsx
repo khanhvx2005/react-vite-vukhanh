4cdd3aa0cdd9ed078a0501fcb0324a89c1ff6a6d
Vu Khanh <kvuxuan789@gmail.com>
2025-11-18 20:36:13 +0700

    vd 30

--- components/learn/MyComponent.jsx
+++ components/learn/MyComponent.jsx
@@ -1,6 +1,11 @@
+import "./style.css"
 const MyComponent = () => {
     return (
-        <div>Thẻ div</div>
+        <>
+            <div className="child" style={{ borderRadius: "8px" }}>Thẻ div</div>
+            <div>Thẻ div 1</div>
+
+        </>
     );
 }
 export default MyComponent;
